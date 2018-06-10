a2fb8814be09ab295296e4ade5f3660ab6058b1e
Aaron Sturm <aaron@holistechsystems.com>
2018-06-09 18:50:18 -0700

    Reduced the number of lines by 'hitting enter' after typing the word

--- randomImage.js
+++ randomImage.js
@@ -10,9 +10,6 @@ const url = 'http://images.google.com';
     const searchBar = await page.$('#lst-ib');
     await searchBar.click();
     await page.keyboard.type(random());
-    const submit = await page.$('#mKlEF');
-    await submit.click();
-    await page.keyboard.type(random());
     await page.keyboard.press('Enter');
     
     await page.$$eval('img', elements => setTimeout(elements[0].click(), 1500));
