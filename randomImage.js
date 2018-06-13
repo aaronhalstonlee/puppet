3881316b4ac715a5e982338ca8292111af433ef3
aaronhalstonlee <aaronhalstonlee@gmail.com>
2018-06-13 12:41:52 -0700

    added download image

--- randomImage.js
+++ randomImage.js
@@ -1,9 +1,11 @@
 const pup = require('puppeteer');
 const random = require('random-words');
+const fs = require('fs');
 const url = 'http://images.google.com';
+const time = new Date();
 
 (async() => {
-    const browser = await pup.launch({headless: false});
+    const browser = await pup.launch();
     const page = await browser.newPage();
     
     await page.goto(url);
@@ -16,13 +18,14 @@ const url = 'http://images.google.com';
     await page.waitForSelector('.rg_ic.rg_i', { timeout: 10000 });
     const firstImg = await page.$$eval('img', elements => elements[1].click());
     
-    await page.waitForSelector('.Tl8XHc', { timeout: 10000 });
-    const firstImgPage = await page.evaluate(() => document.querySelector('.irc_vpl.i3599.irc_lth').href);
-    console.log(firstImgPage);
-    //await page.goto(firstImgPage);    
+    await page.waitForSelector('.irc_mi[src]', { timeout: 10000 });
+    const imgUrl = await page.evaluate(() => document.querySelector('.irc_mi[src]').getAttribute('src'));
+    const viewSource = await page.goto(imgUrl);
     
-    //await page.waitForSelector('#isr_mc', { timeout: 10000 });
-    //await page.evaluate(() => document.querySelector('#isr_mc a'));
-
-    //await browser.close();
+    fs.writeFile(`./randomPic-${time.getMinutes()}.${time.getSeconds()}.jpg`, await viewSource.buffer(), function(err) {
+        if(err) return console.log(err);
+        console.log("file saved");
+    });
+    
+    await browser.close();
 })();
