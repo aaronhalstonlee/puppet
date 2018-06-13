cfa343ec87747b6174f5171fa5dfe42fc9662979
aaronhalstonlee <aaronhalstonlee@gmail.com>
2018-06-13 12:02:17 -0700

    successfully clicks image

--- randomImage.js
+++ randomImage.js
@@ -9,16 +9,20 @@ const url = 'http://images.google.com';
     await page.goto(url);
     const searchBar = await page.$('#lst-ib');
     await searchBar.click();
+
     await page.keyboard.type(random());
     await page.keyboard.press('Enter');
 
-    await page.waitForSelector('#isr_mc', { timeout: 10000 } );
-    const imageUrl = await page.evaluate(() => document.querySelector('#isr_mc a').href );
-    console.log(imageUrl);
+    await page.waitForSelector('.rg_ic.rg_i', { timeout: 10000 });
+    const firstImg = await page.$$eval('img', elements => elements[1].click());
+    
+    await page.waitForSelector('.Tl8XHc', { timeout: 10000 });
+    const firstImgPage = await page.evaluate(() => document.querySelector('.irc_vpl.i3599.irc_lth').href);
+    console.log(firstImgPage);
+    //await page.goto(firstImgPage);    
+    
+    //await page.waitForSelector('#isr_mc', { timeout: 10000 });
+    //await page.evaluate(() => document.querySelector('#isr_mc a'));
 
-    await browser.close();
+    //await browser.close();
 })();
-
-
-
-
